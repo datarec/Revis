90a7c466e3d728db643e1e04856003bde2740e5b
datarec <73265375+datarec@users.noreply.github.com>
2025-01-15 14:21:24 +1100

    Update main.py
    bug fixes and added color.

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import time
 import socket
 import reversed
+from termcolor import colored 
+import subprocess
 
 def main():
 	try:
-		options = input("""
-R e v i s
+		options = input(colored("""R e v i s
 	Hacking made easy.
 
 1) Generate payload and Hack (Partially automated - Simpler)
 2) Generate Payload (Manual - Advanced) 
 
 
->>  """)
+>>  """, "red"))
 
 		if options == "1":
 			pass
 		elif options == "2":
 			reversed.payloadSelection()
 		else:
-			print("Select using 1 or 2. ")
+			print("\nSelect using 1 or 2. ")
 			time.sleep(2)
+			subprocess.run(["cls"], shell=True)
 			main()
 	except KeyboardInterrupt:
-		print("Good-bye!")
+		print(colored("\n\nGood-bye!", "red"))
 
 
 main()
