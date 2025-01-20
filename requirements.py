def8d8fd43beff49435dd8b92096e70cc16aabf5
datarec <73265375+datarec@users.noreply.github.com>
2025-01-20 15:50:28 +1100

    Update requirements.py
    adding ngrok installation command into dependencies.

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 import subprocess
 
 subprocess.run(["pip3", "install", "termcolor"], shell=True, capture_output=True)
+subprocess.run(["winget", "install", "ngrok"], shell=True, capture_output=True)
 
-print("Dependencies installed (:")
+print("\nDependencies installed (:")
