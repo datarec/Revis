634e381f4bc66aefb49589d90da1b48ea99e260f
datarec <73265375+datarec@users.noreply.github.com>
2025-01-22 15:57:29 +1100

    Update automation.py
    adding features

--- automation.py
+++ automation.py
@@ -1,17 +1,28 @@
 import subprocess
 from termcolor import colored
 import threading
+import socket
+import reversed
 
 def start_ngrok_service():
     NEW_CONSOLE = subprocess.CREATE_NEW_CONSOLE
     subprocess.Popen(
-                    ["ngrok", "tcp", "3333"], 
-                     creationflags=NEW_CONSOLE
-    )
-    subprocess.Popen(
-                    ["python", "main.py"] 
+                    ["ngrok", "tcp", "4444"], 
+                     creationflags=NEW_CONSOLE 
     )
 
+def start_listener():
+    try:
+        print("""In the Ngrok window look for the socket. 
+Should look something like this: 0.tcp.au.ngrok.io:12527""")
+        
+        input("Enter: ")
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.bind(("localhost", 4444))
+        listener.listen()
+        ip, data = listener.accept()
+    except KeyboardInterrupt:
+        exit()
 
 def clear_screen():
     subprocess.run(["cls"], shell=True)
@@ -19,7 +30,9 @@ def clear_screen():
 
 def main():
     clear_screen()
-    start_ngrok_service()
+    sng_service = threading.Thread(target=start_ngrok_service)
+    sng_service.start()
+    start_listener()
 
 
 if __name__ == "__main__":
