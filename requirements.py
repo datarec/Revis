6477ea4c5a3bb53e73e5188104c3d845d8215657
datarec <73265375+datarec@users.noreply.github.com>
2025-01-25 14:17:26 +1100

    Update requirements.py
    added dependencies.

--- requirements.py
+++ requirements.py
@@ -1,6 +1,7 @@
 import subprocess
 
+print("Downloading depedencies, could take a minute.")
 subprocess.run(["pip3", "install", "termcolor"], shell=True, capture_output=True)
-subprocess.run(["winget", "install", "ngrok"], shell=True, capture_output=True)
-
-print("\nDependencies installed (:")
+subprocess.run(["winget", "install", "choco"], shell=True, capture_output=True)
+subprocess.run(["choco", "install", "ngrok"], shell=True, capture_output=True)
+print("\nDependencies installed!")
